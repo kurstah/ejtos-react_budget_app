25b09649f7747939a2fcf46e08c68f358df5d9c7
kurstah <nahuelparoldo@hotmail.com>
2023-06-02 04:28:02 -0400

    redi

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -30,6 +30,17 @@ const ExpenseItem = (props) => {
     });
   };
 
+  const decreaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: 10,
+    };
+    dispatch({
+      type: 'RED_EXPENSE',
+      payload: expense,
+    });
+  };
+
   return (
     <tr>
       <td>{props.name}</td>
@@ -39,6 +50,9 @@ const ExpenseItem = (props) => {
       </td>
       <td>
         <button onClick={(event) => increaseAllocation(props.name)}>+</button>
+      </td>
+      <td>
+        <button onClick={(event) => decreaseAllocation(props.name)}>-</button>
       </td>
       <td>
         <TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete>
